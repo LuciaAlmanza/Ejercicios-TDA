38dc1aeba0a944e65541e815b0320e85994a74fb
LuciaAlmanza <luciaalmanzaa@gmail.com>
2024-10-23 17:34:39 -0300

    sub con grafos

---
+++ Backtraking/17.py
@@ -0,0 +1,51 @@
+from grafo import Grafo
+def submarinos(matriz):
+    g = Grafo()
+    submarinos = []
+    for i in range(len(matriz)):
+        for j in range(len(matriz[i])):
+            if matriz[i][j]: submarinos.append((i, j))
+            g.agregar_vertice((i,j))
+            if i-2 >= 0:
+                g.agregar_arista((i-2, j), (i, j))
+                if j-1 >= 0:g.agregar_arista((i-2, j-1), (i, j))
+                if j-2 >= 0: g.agregar_arista((i-2, j-2), (i, j))
+                if j+2 < len(matriz[i-1]):g.agregar_arista((i-2, j+2), (i, j))
+                if j+1 < len(matriz[i-1]):g.agregar_arista((i-2, j+1), (i, j))
+            if i-1 >= 0:
+                g.agregar_arista((i-1, j), (i, j))
+                if j-1 >= 0:g.agregar_arista((i-1, j-1), (i, j))
+                if j-2 >= 0: g.agregar_arista((i-1, j-2), (i, j))
+                if j+1 < len(matriz[i-1]):g.agregar_arista((i-1, j+1), (i, j))
+                if j+2 < len(matriz[i-1]):g.agregar_arista((i-1, j+2), (i, j))
+            if j-2 >= 0: g.agregar_arista((i, j-2), (i, j))
+            if j-1 >= 0: g.agregar_arista((i, j-1), (i, j))
+    return iluminar_submarinos(g, g.obtener_vertices(), 0, [], submarinos, submarinos)
+
+def iluminar_submarinos(g, vertices, ind_vertice, sol_actual, mejor_sol, submarinos):
+    if len(sol_actual) >= len(mejor_sol):
+        return mejor_sol
+    if esSolucion(g, sol_actual, submarinos):
+        return sol_actual[:]
+    if ind_vertice >= len(vertices):
+        return mejor_sol
+    sol_actual.append(vertices[ind_vertice])
+    agregando_vertice = iluminar_submarinos(g, vertices, ind_vertice+1, sol_actual, mejor_sol, submarinos)
+    if len(agregando_vertice) < len(mejor_sol):
+        mejor_sol = agregando_vertice
+    sol_actual.remove(vertices[ind_vertice])
+    quitando_vertice = iluminar_submarinos(g, vertices, ind_vertice+1, sol_actual, mejor_sol, submarinos)
+    if len(quitando_vertice) < len(mejor_sol):
+        mejor_sol = quitando_vertice
+    return mejor_sol
+
+def esSolucion(g, solucion_candidata, submarinos):
+    for sub in submarinos:
+        esta_iluminado = False
+        for v in solucion_candidata:
+            if g.estan_unidos(v, sub) or v == sub:
+                esta_iluminado = True
+                break
+        if not esta_iluminado:
+            return False
+    return True
