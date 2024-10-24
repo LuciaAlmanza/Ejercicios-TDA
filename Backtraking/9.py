d8a0f3c864baba285a1643a2a4be09c666760f31
LuciaAlmanza <luciaalmanzaa@gmail.com>
2024-10-24 11:04:54 -0300

    arreglillo 1

--- Backtraking/9.py
+++ Backtraking/9.py
@@ -1,28 +1,28 @@
-from compatibles import *
-
-def solucion_compatible(horarios):
-    for i in range(len(horarios)):
-        for j in range(i + 1, len(horarios)):
-            if not son_compatibles(horarios[i], horarios[j]):
-                return False
-    return True
-
-def obtener_combinaciones_backtrack(materias, solucion_parcial):
-    if len(materias) == 0:
-        return [solucion_parcial] if solucion_compatible(solucion_parcial) else []
-
-    materia_actual = materias[0]
-    restantes = materias[1:]
-    soluciones = []
-
-    for curso in materia_actual:
-        nueva_solucion = solucion_parcial + [curso]
-        if solucion_compatible(nueva_solucion):
-            soluciones.extend(obtener_combinaciones_backtrack(restantes, nueva_solucion))
-
-    return soluciones
-
 def obtener_combinaciones(materias):
-    if len(materias) == 0:
-        return []
-    return obtener_combinaciones_backtrack(materias, [])
+    resul=[]
+    combinaciones=[]
+
+    return backtracking_materias(materias,resul,combinaciones,0)
+
+
+
+def backtracking_materias(materias,resul,combinaciones,indice):
+    if len(materias)==indice:
+        combinaciones.append(list(resul))
+        return
+    materia=materias[indice]
+    
+
+    for curso in materia:
+        valido=True
+        for posible in resul:
+            if not son_compatibles(curso,posible):
+                valido=False
+                break
+        
+        if valido:
+            resul.append(curso)
+            backtracking_materias(materias,resul,combinaciones,indice+1)
+            resul.pop()
+
+    return combinaciones
