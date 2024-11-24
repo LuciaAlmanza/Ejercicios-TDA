fc5963516240ed750e863128201e7ecfeba7a42b
LuciaAlmanza <luciaalmanzaa@gmail.com>
2024-11-24 18:50:36 -0300

    greedy act

--- parciales/4_3_24.py
+++ parciales/4_3_24.py
@@ -10,27 +10,27 @@
 # dado un grafo, obtener el mínimo Vertex Cover del mismo. Indicar la complejidad del algoritmo implementado, dar un
 # contraejemplo para el algoritmo implementado y justificar por qué el algoritmo implementado es un algoritmo greedy.
 
-def vertex_cover(grafo):
-    cubierto = set()
-    aristas = set()
-
-    # Obtener todas las aristas del grafo
+def vertex_cover_greedy(grafo):
+    vertex_cover = set()
+    aristas = set() # de la forma (v, w)
     for v in grafo.obtener_vertices():
         for w in grafo.adyacentes(v):
-            if (w, v) not in aristas:  # Asegurarse de no duplicar la arista
-                aristas.add((v, w))
-
-    while aristas:
-        # Elegir una arista arbitraria
-        v, w = aristas.pop()
-        # Agregar ambos vértices al conjunto cubierto
-        cubierto.add(v)
-        cubierto.add(w)
-
-        # Eliminar todas las aristas cubiertas por v y w
-        aristas = {e for e in aristas if e[0] != v and e[1] != v and e[0] != w and e[1] != w}
-
-    return cubierto
+            aristas.add((v, w))
+            aristas.add((w, v))
+    #Ahora la estrategia greedy será ir recorriendo todas las aristas y agregar vértices a los extremos de las aristas si estas no tienen
+    # ninguno de sus extremos en el vertex cover. Además entre los 2 elegiremos al que tenga mayor grado
+    #Ejemplo, si tenes la arista (A, B) y A tiene grado 1 y B grado 1000. Obviamente elegiremos a B para agregarlo al VC.
+    visitados = set()
+    for v, w in aristas:
+        if (v, w) in visitados or (w, v) in visitados:
+            continue
+        visitados.add((v, w))
+        if v not in vertex_cover and w not in vertex_cover:
+            if grafo.grado(v) > grafo.grado(w):
+                vertex_cover.add(v)
+            else:
+                vertex_cover.add(w)
+    return vertex_cover
 
 #O(m) numero de aristas del grafo
 
